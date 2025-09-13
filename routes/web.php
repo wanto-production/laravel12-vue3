94c344423a9398512ca37423c3c2da778556be38
wanto-production <ikwansatria3974@gmail.com>
2025-09-13 11:48:31 +0700

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
